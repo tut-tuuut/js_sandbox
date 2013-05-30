6ea16865d387d6f09868d53dc9342c5e0fbaaf6b
Agnès Haasser <ahaasser@tea-ebook.com>
2013-05-30 17:03:05 +0200

    Test with a settimeout

--- touch/script.js
+++ touch/script.js
@@ -3,13 +3,17 @@ var myfunctiononclick = function() {
 }
 
 var observer = function(evt) {
-    console.log(evt);
-    console.log(this);
     evt.stopPropagation();
     evt.preventDefault();
     Element.show('showme');
-    window.location = this.href;
+    var href=this.href;
+    var toto = function() {
+        window.location = href;
+    }
+    setTimeout(toto, 2000);
 };
+
+
 $('observed').observe('click', observer);
 $('obs_touchstart').observe('touchstart', observer);
 $('obs_touchend').observe('touchend', observer);
